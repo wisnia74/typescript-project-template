acf9026ccd03821a14b5c55247e5a93077dbe7af
wisnia74 <41591175+wisnia74@users.noreply.github.com>
2021-06-02 16:35:40 +0200

    Update prettier.config.ts

--- prettier.config.ts
+++ prettier.config.ts
@@ -3,6 +3,7 @@ import type { Options } from 'prettier';
 const config: Options = {
   semi: true,
   tabWidth: 2,
+  printWidth: 120,
   singleQuote: true,
   trailingComma: 'es5',
   endOfLine: 'lf',
