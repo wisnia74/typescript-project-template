9cc51e9c4bc920ba1d8d9892668f07950a922708
wisnia74 <jakub.wisniowski74@gmail.com>
2021-06-15 18:15:56 +0200

    remove prettier types

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from '@jest/types';
-
-const config: Config.InitialOptions = {
-  bail: 1,
-  preset: 'ts-jest',
-};
-
-export default config;
--- script/test.sh
+++ script/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jest $@
+jest --config ./jest.config.json $@
